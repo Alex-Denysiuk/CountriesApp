b38eb839bfb3702be483568240f7adf534069859
Lex Denysiuk <alexd@ipoft.com>
2023-08-19 20:04:54 +0300

    added dependencies

--- CountriesApp/app/build.gradle.kts
+++ CountriesApp/app/build.gradle.kts
@@ -31,11 +31,46 @@ android {
     }
 }
 
+var lifecycleExtensionVersion = "1.1.1"
+var butterKnifeVersion = "10.1.0"
+var supportVersion = "29.0.0"
+var retrofitVersion = "2.3.0"
+var glideVersion = "4.9.0"
+var rxJavaVersion = "2.1.1"
+var daggerVersion = "2.14.1"
+var mockitoVersion = "2.11.0"
+
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.android.support:design:$supportVersion")
+
+    implementation("android.arch.lifecycle:extensions:$lifecycleExtensionVersion")
+
+    implementation("com.jakewharton:butterknife:$butterKnifeVersion")
+    annotationProcessor("com.javkewharton:butterknife-compiler:$butterKnifeVersion")
+
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion")
+
+    implementation("io.reactivex.rxjava2:rxjava:$rxJavaVersion")
+    implementation("io.reactivex.rxjava2:rxandroid:$rxJavaVersion")
+
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    implementation("com.google.dagger:dagger-android-support:$daggerVersion")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    annotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
+    annotationProcessor("com.google.dagger:dagger-android-processor:$daggerVersion")
+
+    testImplementation("org.mockito:mockito-inline:$mockitoVersion")
+    testImplementation("android.arch.core:core-testing:1.1.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

779241979f40b7c74cbb7a56084eb200642163b3
Lex Denysiuk <alexd@ipoft.com>
2023-08-19 20:09:39 +0300

    implemented the model

--- CountriesApp/app/src/main/java/com/alexd/countriesapp/model/CountryModel.java
+++ CountriesApp/app/src/main/java/com/alexd/countriesapp/model/CountryModel.java
@@ -1,4 +1,26 @@
 package com.alexd.countriesapp.model;
 
 public class CountryModel {
+
+    private final String countryName;
+    private final String capital;
+    private final String flag;
+
+    public CountryModel(String countryName, String capital, String flag) {
+        this.countryName = countryName;
+        this.capital = capital;
+        this.flag = flag;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
 }

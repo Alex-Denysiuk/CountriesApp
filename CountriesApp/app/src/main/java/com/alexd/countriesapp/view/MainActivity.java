e4e825fae088b6214afc72ebdb942b04cacad78f
Lex Denysiuk <alexd@ipoft.com>
2023-08-20 19:39:54 +0300

    implement main view

--- CountriesApp/app/src/main/java/com/alexd/countriesapp/view/MainActivity.java
+++ CountriesApp/app/src/main/java/com/alexd/countriesapp/view/MainActivity.java
@@ -1,16 +1,68 @@
 package com.alexd.countriesapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
+import com.alexd.countriesapp.databinding.ActivityMainBinding;
+import com.alexd.countriesapp.model.CountryModel;
+import com.alexd.countriesapp.viewModel.ListViewModel;
 
-import com.alexd.countriesapp.R;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private ListViewModel viewModel;
+    private final CountryListAdapter adapter = new CountryListAdapter(new ArrayList<>());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = this.binding.getRoot();
+        setContentView(view);
+
+        this.viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+        this.viewModel.refresh();
+
+        this.binding.rvCountriesList.setLayoutManager(new LinearLayoutManager(this));
+        this.binding.rvCountriesList.setAdapter(adapter);
+
+        this.observeViewModel();
+    }
+
+    private void observeViewModel() {
+        this.viewModel.countries.observe(this, countryModels -> {
+            if(countryModels == null) {
+                return;
+            }
+            binding.rvCountriesList.setVisibility(View.VISIBLE);
+            List<ItemViewModel> items = new ArrayList<>();
+            for (CountryModel model :
+                    countryModels) {
+                items.add(new CountryItemViewModel(model));
+            }
+            adapter.updateCountries(items);
+            binding.rvCountriesList.post(adapter::notifyDataSetChanged);
+        });
+        this.viewModel.countryLoadError.observe(this, isError -> {
+            if(isError == null) {
+                return;
+            }
+            binding.tvListError.setVisibility(isError ? View.VISIBLE : View.GONE);
+        });
+        this.viewModel.loading.observe(this, isLoading -> {
+            if(isLoading == null) {
+                return;
+            }
+            binding.pbLoaginView.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+            if(isLoading) {
+                binding.tvListError.setVisibility(View.GONE);
+                binding.rvCountriesList.setVisibility(View.GONE);
+            }
+        });
     }
 }

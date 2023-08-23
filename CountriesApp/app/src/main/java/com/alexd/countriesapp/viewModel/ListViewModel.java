c95b8025702e8b7332da530b42c1fd6d80ecf7f0
Lex Denysiuk <alexd@ipoft.com>
2023-08-23 21:52:39 +0300

    added ability to get data from backend

--- CountriesApp/app/src/main/java/com/alexd/countriesapp/viewModel/ListViewModel.java
+++ CountriesApp/app/src/main/java/com/alexd/countriesapp/viewModel/ListViewModel.java
@@ -1,12 +1,21 @@
 package com.alexd.countriesapp.viewModel;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.alexd.countriesapp.model.CountriesService;
 import com.alexd.countriesapp.model.CountryModel;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class ListViewModel extends ViewModel {
 
@@ -14,21 +23,41 @@ public class ListViewModel extends ViewModel {
     public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<>();
 
+    private CountriesService countriesService = CountriesService.getInstance();
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+
     public void refresh() {
         this.fetchCountries();
     }
 
     private void fetchCountries() {
-        CountryModel countryModel1 = new CountryModel("Albania", "Tirana", "");
-        CountryModel countryModel2 = new CountryModel("Brazil", "Brasilia", "");
-        CountryModel countryModel3 = new CountryModel("Czechia", "Praga", "");
+        loading.setValue(true);
+        this.disposable.add(this.countriesService.getCountries()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<CountryModel>>() {
+                            @Override
+                            public void onSuccess(List<CountryModel> countryModels) {
+                                countries.setValue(countryModels);
+                                countryLoadError.setValue(false);
+                                loading.setValue(false);
+                            }
 
-        List<CountryModel> countryModels = new ArrayList<>();
-        countryModels.add(countryModel1);
-        countryModels.add(countryModel2);
-        countryModels.add(countryModel3);
-        countries.setValue(countryModels);
-        countryLoadError.setValue(false);
-        loading.setValue(false);
+                            @Override
+                            public void onError(Throwable e) {
+                                countryLoadError.setValue(true);
+                                loading.setValue(false);
+                                e.printStackTrace();
+                            }
+                        })
+        );
+
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        this.disposable.clear();
     }
 }
